07f7c97e8fd64c9c89f1a4871917c09761f07e6a
RM49 <108128974+RM49@users.noreply.github.com>
2023-01-15 15:54:10 +0000

    Add files via upload

---
+++ day 7/problem 2.py
@@ -0,0 +1,69 @@
+# done after a while without trees
+file = open("data.txt", "r")
+data = file.readlines()
+
+# all directories in a list. Index 1 of each list is the name of the directory, each list is a directory.
+system = ["/"]
+
+data.pop(0) # get rid of cd /
+d = []
+for l in data:
+    d.append(l.strip("\n").split(" "))
+    
+# d is the list of commands / data
+
+path = [] # route of where the stuff being handled is. Each will point to a directory in each sub list.
+
+for l in d:
+
+    # pointing to current directory list
+    current = system
+    for n in path:
+        current = current[n]
+
+
+    if l[0] == "$":
+        if l[1] == "ls":
+            continue
+        if l[1] == "cd":
+            if l[2] == "..":
+                if path != []:
+                    path.pop()
+            else: # cd </a>, finds which index the directory is at then adds to path
+                for directories in current:
+                    if type(directories) == list:
+                        if directories[0] == l[2]:
+                            path.append(current.index(directories))
+    elif l[0] == "dir":
+        current.append([l[1]])
+    else:
+        current.append([int(l[0]), l[1]])
+totals = []
+def count(directory):
+    total = 0
+    for d in directory:     
+        if type(d[0]) == int:
+            total += d[0]
+        elif type(d[0]) == str and d[0] != "/":
+            d.pop(0) # stops infinite loop on directory name
+            temp = count(d)
+            print(temp)
+            total += temp
+            totals.append(temp)
+    return total
+
+print(system)
+total_mem = count(system)
+print(total_mem)
+unused = 70000000 - total_mem
+required = 30000000 - unused
+
+dirs_with_enough = []
+for t in totals:
+    if t > required:
+        dirs_with_enough.append(t)
+dirs_with_enough.sort()
+print(dirs_with_enough[0])
+
+
+
